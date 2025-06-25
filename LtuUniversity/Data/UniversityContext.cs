804801cb38511f0fa568bd6b4a5f4ef2706bac57
DZM77 <dimitris.bjorlingh@outlook.com>
2025-06-25 13:53:45 +0200

    After Scaffold

---
+++ LtuUniversity/Data/UniversityContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LtuUniversity.Models.Entities;
+
+namespace LtuUniversity.Data
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext (DbContextOptions<UniversityContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; } = default!;
+    }
+}
--- LtuUniversity/Program.cs
+++ LtuUniversity/Program.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using LtuUniversity.Data;
 
 namespace LtuUniversity
 {
@@ -6,14 +9,22 @@ namespace LtuUniversity
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            
             // Add services to the container.
+            
+            builder.Services.AddDbContext<UniversityContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("UniversityContext") ?? throw new InvalidOperationException("Connection string 'UniversityContext' not found.")));
+
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
             var app = builder.Build();
+
+
+
+
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
