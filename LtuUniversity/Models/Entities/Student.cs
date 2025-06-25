3b5697d61437882677f641a9f97b2652f591f072
DZM77 <dimitris.bjorlingh@outlook.com>
2025-06-25 13:50:30 +0200

    Add Student

---
+++ LtuUniversity/Models/Entities/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace LtuUniversity.Models.Entities;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string FullName => $"{FirstName} {LastName}";
+}
